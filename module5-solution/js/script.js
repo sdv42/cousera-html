0654aa6d540e3fc6aa5de9f5c5707f0a8a2ce15e
dsv <my@mail.test>
2016-05-30 22:21:52 +0300

    fix a typo

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage =
         insertProperty(homeHtml,
                        "randomCategoryShortName",
-                       "'" + chooseRandomCategory + "'")
+                       "'" + chosenCategoryShortName + "'")
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
